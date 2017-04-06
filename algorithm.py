2ab2da76ab4a323079417e8280799d8f2bbefc4a
WilliamLewww <WilliamLew32@gmail.com>
2017-04-05 21:34:43 -0700

    multi-thread update database

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,25 @@
 import urllib2
+import Queue
+import threading
 from stock import *
 from yahoo_finance import Share
 
 stock_database = []
 
-def update_database():
+def update_database(start_at, end_at, queue):
+    temp_database = []
     with open('symbols.txt') as file:
-        for x in range(5):
+        for x in range(start_at):
+            file.readline()
+        for x in range(end_at - start_at):
             stock = Share(file.readline().split('|')[0])
-            if any(x.name == file.readline().split('|')[0] for x in stock_database):
-                stock_database[x].price_list.append(stock.get_price())
+            if any(x.name == file.readline().split('|')[0] for x in temp_database):
+                temp_database[x].price_list.append(stock.get_price())
             else:
-                stock_database.append(Stock(file.readline().split('|')[0]))
-                stock_database[len(stock_database) - 1].append_price(stock.get_price())
+                temp_database.append(Stock(file.readline().split('|')[0]))
+                temp_database[len(temp_database) - 1].append_price(stock.get_price())
+
+    queue.put(temp_database)
 
 def update_stock_list():
     url = urllib2.urlopen('ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt')
@@ -22,6 +29,8 @@ def update_stock_list():
     file.write(data)
     file.close()
 
-update_stock_list()
-update_database()
-print(len(stock_database))
+queue = [Queue.Queue(), Queue.Queue()]
+threading.Thread(target = update_database, args = (0, 5, queue[0])).start()
+threading.Thread(target = update_database, args = (5, 12, queue[1])).start()
+print(len(queue[0].get()))
+print(len(queue[1].get()))
